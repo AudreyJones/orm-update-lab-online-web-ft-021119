1daafc286c71f4a8816ece395c174e8b2658a252
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-24 16:52:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,8 +39,10 @@ class Student
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def self.create
-
+  def self.create(name, grade)
+    student = Student.new(name,grade)
+    student.save
+    student
   end
 
 end
