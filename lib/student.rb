71a611d7dd673c7b08543b5ba5b7e0dfc83d6fdd
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-24 16:19:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
 
-  def initialize(name:,grade:)
+  def initialize(name, grade)
     @name = name
     @grade = grade
   end
