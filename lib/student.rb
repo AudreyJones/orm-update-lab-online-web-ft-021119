71d17530e212bd10de32604a9b6e4dde53d77494
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-24 16:25:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NONE ELSE EXIST students (
+      CREATE TABLE IF NOT ELSE EXIST students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
